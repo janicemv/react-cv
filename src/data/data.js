67c6a48b3047ca949db2c16600390eff09586dff
Janice <janice.vilela@gmail.com>
2025-03-13 22:18:45 +0100

    added experiences and skills

--- src/data/data.js
+++ src/data/data.js
@@ -40,15 +40,15 @@ I love automating processes, making information accessible, and structuring data
           description: 'PHP, SQL, databases, HTML, CSS, Bootstrap, JavaScript, Laravel, React, Node, Tailwind, communication training, implementing and using Scrum for project management.',
           companyWebSite: 'http://www.vdab.be',
           datesBetween: '05/2024 - 02/2025',
-          descriptionTags: ['HTML', 'CSS', 'Javascript', 'React','PHP', 'SQL', 'Laravel','Scrum', 'Agile', 'Bootstrap', 'Tailwind', 'Blade'],
+          descriptionTags: ['HTML', 'CSS', 'Javascript', 'React', 'PHP', 'SQL', 'Laravel', 'Scrum', 'Agile', 'Bootstrap', 'Tailwind', 'Blade'],
         },
         {
           title: 'Harvard CS50: Introduction to Computer Science',
           company: 'Harvard Online/EdX',
-          description: 'An introduction to the intellectual enterprises of computer science and the art of programming',
+          description: 'Resource Management, Python (Programming Language), HyperText Markup Language (HTML), C (Programming Language), Cryptography, Security Software, Forensic Sciences, SQL (Programming Language), Data Structures, Cascading Style Sheets (CSS), Algorithms, Finance, JavaScript (Programming Language), Computer Science',
           companyWebSite: 'https://www.edx.org/learn/computer-science/harvard-university-cs50-s-introduction-to-computer-science',
           datesBetween: '12/2023 - 05/2024',
-          descriptionTags: ['HTML', 'CSS', 'Javascript', 'C','Python', 'SQL'],
+          descriptionTags: ['HTML', 'CSS', 'Javascript', 'C', 'Python', 'SQL'],
         },
         {
           title: 'Blasting Off with Bootstrap',
@@ -94,7 +94,27 @@ I love automating processes, making information accessible, and structuring data
           description: 'Postdoc with WBI grant (2018-2020); Funded by Innoviris Proof of Concept (2021-2022); Research on fertility preservation in women',
           companyWebSite: 'https://www.researchgate.net/lab/Pole-de-Recherche-en-Physiopathologie-de-la-Reproduction-REPR-Christiani-Andrade-Amorim',
           datesBetween: '11/2018 - 08/2022',
-          descriptionTags: ['Research', 'Data analysis', 'Statistics', 'Scientific writing', 'Presentations'],
+          descriptionTags: ['Research', 'Data analysis', 'Statistics', 'Project Management', 'Scientific writing', 'Presentations'],
+        },
+        {
+          title: 'Full Collaborator Researcher',
+          company: 'Universidade de Brasília (UnB, Brazil)',
+          description: 'Postdoctoral Program in Nanoscience and Nanobiotechnology',
+          datesBetween: '2017 - 2019',
+        },
+        {
+          title: 'Administrative Assistant',
+          company: 'Coordenação de Aperfeiçoamento de Pessoal de Nível Superior (CAPES, Brazil)',
+          description: 'Department of International Relations - Managed the migration of form data from universities of the whole country',
+          companyWebSite: 'https://www.gov.br/capes/pt-br',
+          datesBetween: '2017 - 2018',
+        },
+        {
+          title: 'Professor',
+          company: 'Faculdades Integradas da União Educacional do Planalto Central (FACIPLAC,  Brazil)',
+          description: 'Physiology, Immunology and Microbiology',
+          companyWebSite: 'https://www.uniceplac.edu.br/',
+          datesBetween: '2014',
         },
         {
           title: 'Webmaster',
@@ -103,7 +123,20 @@ I love automating processes, making information accessible, and structuring data
           companyWebSite: 'https://www.bioloja.bio.br',
           datesBetween: '2013 - 2017',
           descriptionTags: ['Wordpress', 'Microsoft Office', 'Presentations', 'Teaching materials'],
-        }
+        },
+        {
+          title: 'Wild Animal Care (volunteer)',
+          company: 'Instituto Brasileiro do Meio Ambiente e dos Recursos Naturais Renováveis (IBAMA, Brazil)',
+          description: 'Clinic, Surgery and Animal Care – wild birds, mammals, small primates',
+          datesBetween: '2007 / 2009',
+        },
+        {
+          title: 'Rescuing of Marine Animals (volunteer)',
+          company: 'Instituto de Pesquisas Cananéia (IPeC, Brazil)',
+          description: 'Beach monitoring, rescue and care of grounding penguins',
+          datesBetween: '2008',
+        },
+
       ]
     },
     {
@@ -119,16 +152,18 @@ I love automating processes, making information accessible, and structuring data
           ]
         },
         {
-          sectionHeader: 'Fertility Research',
-          description: 'Created during my academic years',
+          sectionHeader: 'Academic Research',
+          description: 'My publications during my academic years',
           items: [
             { title: 'Fertility Database', projectUrl: 'https://airtable.com/appfO1qRNoGz3DRsZ/shr9eoxd68mFQCDR1', description: 'Airtable database created for my systematic review' },
-            { title: 'The Review', projectUrl: 'https://www.rbmojournal.com/article/S1472-6483(20)30586-1/fulltext', description: 'Published review with the data' },
+            { title: 'Research Gate', projectUrl: 'https://www.researchgate.net/profile/Janice-Vilela-2', description: 'My publications' },
+            { title: 'Presentations and Multimedia', projectUrl: 'https://drive.google.com/drive/folders/1CcDVCIhpiEtQTQoPSOHlcDEU1RkONvtu', description: 'Materials of lessons as a professor' },
           ]
+
         },
       ]
     },
-    
+
     {
       type: 'common-list',
       title: 'Languages',
@@ -183,7 +218,86 @@ I love automating processes, making information accessible, and structuring data
           datesBetween: '2004-2010',
         },
       ]
-      },
+    },
+    {
+      type: 'experiences-list',
+      title: 'Certificates',
+      description: '',
+      icon: 'graduation',
+      items: [
+        {
+          title: 'Interdisciplinary Program in Healthcare Innovation (I³H)',
+          company: 'Université Libre de Bruxelles',
+          companyWebSite: 'https://i3health.eu/i3h-education/',
+          description: 'Basic knowledge and a holistic view of the complex processes that translate scientific advances into new standards of care. Addresses the scientific, commercial and social issues of modern health.',
+          datesBetween: '2020',
+        },
+        {
+          title: 'Ovarian tissue freezing and thawing',
+          company: 'Cliniques Universitaires St Luc, UCL, Belgium',
+          description: 'Training of laboratory technologist personnel for the freezing and thawing of ovarian tissue',
+          datesBetween: '2020',
+        },
+        {
+          title: 'Workshop – Marketing for Scientists',
+          company: 'Université Catholique de Louvain',
+          companyWebSite: 'https://marketingforscientists.com/',
+          description: 'Ministered by Marc Kuchner  - 7h duration',
+          datesBetween: '2019',
+        },
+        {
+          title: 'FELASA B & C Certificates',
+          company: 'Université Catholique de Louvain',
+          description: 'Science and medicine of laboratory animals',
+          datesBetween: '2018 - 2019',
+        },
+      ]
+    },
+    {
+      type: 'common-list',
+      title: 'Skills',
+      icon: 'rocket',
+      items: [
+        {
+          authority: 'Goal-directed and results-oriented professional',
+        },
+        {
+          authority: 'Fast learner with strong writing skills: able to grasp new concepts and articulate them effectively',
+        },
+        {
+          authority: 'Leadership and bridge-building: skilled in conflict mediation, team guidance, and fostering a collaborative culture',
+        },
+        {
+          authority: 'Ability to absorb, analyse and interpret detailed data and information',
+        },
+        {
+          authority: 'Effective team player: collaborates seamlessly with colleagues to drive projects forward in shared areas of expertise',
+        },
+        {
+          authority: 'Expertise in researching, training, writing, documentation, and structured organization',
+        },
+
+        {
+          authority: 'Adaptable and comfortable working in cross-cultural environments',
+        },
+        {
+          authority: 'Driven and results-focused: takes initiative and maintains a sharp focus on objectives',
+        },
+        {
+          authority: 'Creative problem-solving and idea fusion: able to think beyond conventional boundaries and link insights effectively',
+        },
+        {
+          authority: 'Resilient mindset: consistently overcomes obstacles with determination and strategic thinking',
+        },
+        {
+          authority: 'Effective knowledge transfer: experienced in teaching and simplifying complex ideas for diverse audiences',
+        },
+
+
+
+
+      ]
+    },
     // {
     //   type: 'tag-list',
     //   title: 'Hobbies & Interests',
